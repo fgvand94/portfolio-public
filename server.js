d84be6fb4a1b22e1c296a8274b650a8be1ce5075
Forrest Van Duren <forrestvanduren1@gmail.com>
2022-03-27 00:45:27 -0600

    test

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 
-// app.use(express.static('public'));
-// app.use(express.json());
+app.use(express.static('public'));
+app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + "public/index.html")
-// });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "public/index.html")
+});
 
 
 
